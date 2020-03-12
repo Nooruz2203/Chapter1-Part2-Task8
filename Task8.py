9a034f4782169202b8a7a1a51e78b5e88504c003
Nooruz2203 <noracom91@gmail.com>
2020-03-12 14:00:06 +0600

    first commit

---
+++ Task8.py
@@ -0,0 +1,9 @@
+print ("Enter the first integer:")
+A = int(input())
+print ("Enter the second integer:")
+B = int(input())
+A<=B
+diapozon = range(A,B) 
+print(diapozon)
+new_list = list(diapozon)
+print((new_list),(B))
